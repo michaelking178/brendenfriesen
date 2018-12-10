7383d787a12b58c0b3ddb1a0d47e9393db8b642a
Michael King <mack178@hotmail.com>
2018-12-10 12:32:34 -0500

    Add GET routes and create header and footer partials

--- app.js
+++ app.js
@@ -13,6 +13,22 @@ app.get("/index", function(req, res){
     res.render("index");
 });
 
+app.get("/bio", function(req, res){
+    res.render("bio");
+});
+
+app.get("/calendar", function(req, res){
+    res.render("calendar");
+});
+
+app.get("/press", function(req, res){
+    res.render("press");
+});
+
+app.get("/contact", function(req, res){
+    res.render("contact");
+});
+
 // Port listening
 app.listen(3000, function(){
     console.log("Server running...");
