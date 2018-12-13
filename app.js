7b08868eca64ee558b3dd83850e88c1f4308831d
Michael King <mack178@hotmail.com>
2018-12-13 18:47:51 -0500

    Adjust port listening for deployment

--- app.js
+++ app.js
@@ -30,6 +30,8 @@ app.get("/contact", function(req, res){
 });
 
 // Port listening
-app.listen(3000, function(){
-    console.log("Server running...");
-});
+let port = process.env.PORT;
+if (port == null || port == ""){
+    port = 8000;
+}
+app.listen(port);
