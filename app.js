be9f7fa55f14041745d9b893ad9175dee34e1966
Michael King <mack178@hotmail.com>
2019-01-02 15:03:04 -0500

    Minor adjustments to SendGrid settings

--- app.js
+++ app.js
@@ -50,8 +50,8 @@ app.post("/send", function(req, res){
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
         host: 'smtp.sendgrid.net',
-        port: 587,
-        secure: false, // true for 465, false for other ports
+        port: 465,
+        secure: true, // true for 465, false for other ports
         auth: {
             user: sendGrid.user, // SendGrid API
             pass: sendGrid.pass // SendGrid key
