cbf63dee2e88870fc0d8531de27abecf36cb2fda
Michael King <michael.ac.king@outlook.com>
2019-09-08 18:32:50 -0400

    Develop admin and config pages

--- app.js
+++ app.js
@@ -2,12 +2,33 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
 const sendGrid = require("./api");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const passportLocalMongoose = require("passport-local-mongoose");
+const User = require("./models/user");
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(
+    require("express-session")({
+        secret: "Brenden Frisen is a Saskatchewan bass-baritone",
+        resave: false,
+        saveUninitialized: false
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+mongoose.connect("mongodb://localhost/auth_demo_app", {
+    useNewUrlParser: true
+});
 
 //------------------|
 //----- ROUTES -----|
@@ -15,58 +36,63 @@ app.use(bodyParser.json());
 
 // Main Routes
 app.get("/", function(req, res) {
-  res.redirect("/index");
+    res.redirect("/index");
 });
 
 app.get("/index", function(req, res) {
-  res.render("index");
+    res.render("index");
 });
 
 app.get("/bio", function(req, res) {
-  res.render("bio");
+    res.render("bio");
 });
 
 app.get("/calendar", function(req, res) {
-  res.render("calendar");
+    res.render("calendar");
 });
 
 app.get("/contact", function(req, res) {
-  res.render("contact", { msg: "" });
+    res.render("contact", { msg: "" });
 });
 
 app.get("/admin", function(req, res) {
-  res.render("admin");
+    res.render("admin");
 });
 
 // Media Routes
 app.get("/media/audio", function(req, res) {
-  res.render("media/audio");
+    res.render("media/audio");
 });
 
 app.get("/media/video", function(req, res) {
-  res.render("media/video");
+    res.render("media/video");
 });
 
 app.get("/media/images", function(req, res) {
-  res.render("media/images");
+    res.render("media/images");
 });
 
 // Repertoire Routes
 app.get("/repertoire/opera", function(req, res) {
-  res.render("repertoire/opera");
+    res.render("repertoire/opera");
 });
 
 app.get("/repertoire/concert", function(req, res) {
-  res.render("repertoire/concert");
+    res.render("repertoire/concert");
 });
 
 app.get("/repertoire/recital", function(req, res) {
-  res.render("repertoire/recital");
+    res.render("repertoire/recital");
+});
+
+// Admin routes
+app.get("/admin/config", function(req, res) {
+    res.render("admin/config");
 });
 
 // Contact Form Route
 app.post("/send", function(req, res) {
-  const output = `
+    const output = `
         <p>You have a new contact request</p>
         <h3>Contact Details</h3>
         <ul>
@@ -78,44 +104,44 @@ app.post("/send", function(req, res) {
         <p>${req.body.message}</p>
     `;
 
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "smtp.sendgrid.net",
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-      user: sendGrid.user, // SendGrid API
-      pass: sendGrid.pass // SendGrid key
-    },
-    tls: {
-      rejectUnauthorized: false
-    }
-  });
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: "smtp.sendgrid.net",
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+            user: sendGrid.user, // SendGrid API
+            pass: sendGrid.pass // SendGrid key
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
 
-  // setup email data with unicode symbols
-  let mailOptions = {
-    from: '"Brenden Friesen Official Website" <noreply@brendenfriesen.com>', // sender address
-    to: "brendenfriesen@gmail.com", // list of receivers
-    subject: "New Contact Form Message", // Subject line
-    text: "", // plain text body
-    html: output // html body
-  };
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Brenden Friesen Official Website" <noreply@brendenfriesen.com>', // sender address
+        to: "brendenfriesen@gmail.com", // list of receivers
+        subject: "New Contact Form Message", // Subject line
+        text: "", // plain text body
+        html: output // html body
+    };
 
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return console.log(error);
-    }
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log("Message sent: %s", info.messageId);
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 
-    res.render("contact", { msg: "Your message has been sent." });
-  });
+        res.render("contact", { msg: "Your message has been sent." });
+    });
 });
 
 // Port listening
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 8000;
+    port = 8000;
 }
 app.listen(port);
